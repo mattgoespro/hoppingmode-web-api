c94da83b61108d567c8aa97fb71e15ee2f03ed2f
Matt Young <mattyoung305@gmail.com>
2022-01-31 15:40:45 +0200

    cp

--- src/app.ts
+++ src/app.ts
@@ -7,9 +7,38 @@ import {
   PinnedRepositories,
 } from "./app.model";
 import path from "path";
+import { Buffer } from "buffer";
+import axios from "axios";
+import morgan from "morgan";
+
+function sortedRepos(githubRepos: GithubProject[], pinned: PinnedRepositories) {
+  return githubRepos
+    .map((githubRepo) => ({
+      ...githubRepo,
+      pinned: pinned.user.pinnedItems.nodes
+        .map((p) => p.name)
+        .includes(githubRepo.name),
+    }))
+    .sort((repo1, repo2) => {
+      return (repo1.pinned ? 1 : -1) - (repo2.pinned ? 1 : -1);
+    });
+}
+
+function error(resp: Response, status: number, message: string) {
+  return resp.status(status).json({
+    reason: message,
+  });
+}
+
+function setHeaders(_request: Request, response: Response, next: NextFunction) {
+  response.set("Access-Control-Allow-Origin", "localhost");
+  next();
+}
 
 (async function main() {
-  const apiServer = express();
+  const api = express();
+
+  api.use(setHeaders, morgan("[:date[web]] - [:method] :url [:status]"));
 
   const client = new GraphQLClient(env.githubGraphql, {
     headers: {
@@ -18,69 +47,65 @@ import path from "path";
     },
   });
 
-  apiServer.get("/", (request: Request, response: Response) => {
+  api.get("/", (_request: Request, response: Response) => {
     response.sendFile(path.join(__dirname, "../public/index.html"));
   });
 
-  apiServer.get("/repos", (request: Request, response: Response) => {
-    fetch(`${env.githubApi}/users/${env.githubLogin}/repos`)
-      .then<GithubProject[]>((rsp) => rsp.json())
-      .then((repos) => {
-        client.request(pinnedGithubReposRequest).then(
-          (resp: PinnedRepositories) => {
-            console.log(JSON.stringify(resp.user.pinnedItems.nodes));
-            response.set("Access-Control-Allow-Origin", "*");
-            response.json(
-              repos
-                .map((repo) => ({
-                  ...repo,
-                  pinned: resp.user.pinnedItems.nodes
-                    .map((p) => p.name)
-                    .includes(repo.name),
-                }))
-                .sort((a, b) => {
-                  return (b.pinned ? 1 : -1) - (a.pinned ? 1 : -1);
-                })
-            );
+  api.get("/repos", (_request: Request, response: Response) => {
+    axios
+      .get<GithubProject[]>(`${env.githubApi}/users/${env.githubLogin}/repos`)
+      .then((githubRepos) => {
+        client.request<PinnedRepositories>(pinnedGithubReposRequest).then(
+          (pinnedGithubRepos) => {
+            response
+              .status(200)
+              .json(sortedRepos(githubRepos.data, pinnedGithubRepos));
           },
           (rejection) => {
-            console.log(rejection);
             response.json(JSON.stringify(rejection));
+            error(response, 500, rejection);
           }
         );
       });
   });
 
-  apiServer.get(
-    "/repos/:name/languages",
-    (request: Request, response: Response) => {
-      fetch(`${env.githubApi}/${request.params.name}/languages`)
-        .then((resp) => resp.json())
-        .then((languages) => response.json(languages));
-    }
-  );
+  api.get("/repos/:name/languages", (request: Request, response: Response) => {
+    axios
+      .get<{ [key: string]: number }>(
+        `${env.githubApi}/${encodeURIComponent(request.params.name)}/languages`
+      )
+      .then((languages) => {
+        response.status(200).json(languages);
+      });
+  });
 
-  apiServer.get(
-    "/repos/:name/readme",
-    (request: Request, response: Response) => {
-      fetch(
-        `${env.githubApi}/repos/${env.githubLogin}/${request.params.name}/contents/README.md`
-      ).then((rsp) => {
+  api.get("/repos/:name/readme", (request: Request, response: Response) => {
+    axios
+      .get<{ content: string }>(
+        `${env.githubApi}/repos/${env.githubLogin}/${encodeURIComponent(
+          request.params.name
+        )}/contents/README.md`
+      )
+      .then((rsp) => {
         if (rsp.status === 404) {
-          response.send("No Readme found.");
+          error(response, 404, "No Readme found");
         } else if (rsp.status === 200) {
-          rsp.json().then((rsp) => {
-            const readmePayload = Buffer.from(rsp.content, "base64").toString();
-            response.send(readmePayload.trim());
-          });
+          const readmePayload = Buffer.from(
+            rsp.data.content,
+            "base64"
+          ).toString();
+          response.status(200).send(readmePayload.trim());
         } else {
-          response.sendStatus(500);
+          error(
+            response,
+            500,
+            "An unexpected error occurred. Please try again."
+          );
         }
       });
-    }
-  );
+  });
 
-  apiServer.listen(env.port, () => {
+  api.listen(env.port, () => {
     console.log("=".repeat(50));
     console.log("=");
     console.log(`=   API listening on port ${env.port}...`);
