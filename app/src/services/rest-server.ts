0bfc166e70eece308fb759c1eb472ea64827de0b
Matt Young <mattyoung305@gmail.com>
2022-06-25 13:40:48 +0200

    Adding HTTP 304 Response to logger

--- app/src/services/rest-server.ts
+++ app/src/services/rest-server.ts
@@ -6,12 +6,12 @@ morgan.token("status-name", (_req, res) => {
   switch (res.statusCode) {
     case 200:
       return "OK";
-    case 404:
-      return "Not Found";
-    case 301:
-      return "Moved Permanently";
+    case 304:
+      return "Not Modified";
     case 401:
       return "Forbidden";
+    case 404:
+      return "Not Found";
     case 500:
       return "Internal Server Error";
     case 503:
